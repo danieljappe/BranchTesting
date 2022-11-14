cc031220b8a0958a1dd0c55480da6cef335f3858
danieljappe <danieljappe@icloud.com>
2022-11-14 13:03:09 +0100

    makeSound

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,11 @@
 abstract public class Animal {
     private String name;
 
+    public abstract void makeSound();
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
+
+
